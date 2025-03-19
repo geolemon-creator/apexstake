7483391e6887247238913bb6ba62c7bd8d5f1db3
Алексей <sudosurebootapt@gmail.com>
2025-03-19 10:14:52 +0100

    Added: MainPage levels icon, btns border fix

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,69 +1,65 @@
-import { NavLink } from "react-router-dom"
-import box from "./../Img/box.png"
-import coin from "./../Img/coin.png"
+import { NavLink } from "react-router-dom";
+import box from "./../Img/present.svg";
+import arrowRight from "./../Img/arrow-right.svg";
+import arrowDown from "./../Img/arrow-down.svg";
+
+import coin from "./../Img/coin.png";
 
 export default function Home() {
-
   return (
     <div className="home-countainer">
-
-        <div className="home-top-catlog">
-
-            <div className="home-user">
-                <div className="user-icon"></div>
-                <p className="home-user-p">UserName</p>
-            </div>
-
-            <div className="home-level">
-                <p>Выбор уровня</p>
-            </div>
-
+      <div className="home-top-catlog">
+        <div className="home-user">
+          <div className="user-icon"></div>
+          <p className="home-user-p">UserName</p>
         </div>
 
-        <div className="home-lower-catalog">
-            <div>
-                <div className="home-reward">
-
-                    <div className="rewad-text">
-                        <h1 className="reward-h1">Ежедневная награда</h1>
-                        <p className="reward-p">Успей забрать</p>
-                    </div>
-                    
-                    <img className="reward-img" src={box} alt="img" />
-
-                </div>
-            </div>
+        <div className="home-level">
+          <p className="level-p">Выбор уровня</p>
+          <span className="arrow">&gt;</span>
         </div>
+      </div>
 
-        <div className="home-balance">
-
-            <div className="current-balance">
-                <p className="current-balance-p">Current Balance</p>
-                <p className="current-balance-num">$ 0.00</p>
+      <div className="home-lower-catalog">
+        <div>
+          <div className="home-reward">
+            <div className="rewad-text">
+              <h1 className="reward-h1">Ежедневная награда</h1>
+              <p className="reward-p">Успей забрать</p>
             </div>
 
-            <div className="home-bonus">
-                <p className="home-bonus-p">Bonus</p>
+            <img className="reward-img" src={box} alt="img" />
+          </div>
+        </div>
+      </div>
 
-                <div className="bonus-amount">
-                    <img className="img-coin" src={coin} alt="coin" />
-                    <p className="bonus-balance-num">0.00</p>
-                </div>
-                
-            </div>
+      <div className="home-balance">
+        <div className="current-balance">
+          <p className="current-balance-p">Current Balance</p>
+          <p className="current-balance-num">$ 0.00</p>
         </div>
 
-        <div className="home-info-div">
-            <p className="home-info">Данные появятся после начала стейкинга</p>
+        <div className="home-bonus">
+          <p className="home-bonus-p">Bonus</p>
+
+          <div className="bonus-amount">
+            <img className="img-coin" src={coin} alt="coin" />
+            <p className="bonus-balance-num">0.00</p>
+          </div>
         </div>
+      </div>
 
-        <button className="btn-more">Узнать больше</button>
-        <NavLink to="/staking">
-            <button className="btn-staking">Открыть стейкинг</button>
-        </NavLink>
-        
-        
+      <div className="home-info-div">
+        <p className="home-info">Данные появятся после начала стейкинга</p>
+      </div>
 
+      <div className="btn-more">
+        <p>Узнать больше</p>
+        <img src={arrowRight} alt="arrow" />
+      </div>
+      <NavLink style={{ textDecoration: "none" }} to="/staking">
+        <div className="btn-staking">Открыть стейкинг</div>
+      </NavLink>
     </div>
-  )
+  );
 }
--- src/Page/Staking.tsx
+++ src/Page/Staking.tsx
@@ -1,83 +1,91 @@
 import { useState } from "react";
-import deposit from "./../Img/Deposit.png"
-import withdraw from "./../Img/Withdraw.png"
+import deposit from "./../Img/Deposit.png";
+import withdraw from "./../Img/Withdraw.png";
 import { level } from "../Components/Data";
 import Level from "../Components/Level";
 
-
 export default function Staking() {
-    const [isModalOpen, setIsModalOpen] = useState<boolean>(false); 
-    const [selectedLevel, setSelectedLevel] = useState<number | null>(null); 
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [selectedLevel, setSelectedLevel] = useState<number | null>(null);
 
   const handleOpenModal = () => {
-    setIsModalOpen(true); 
+    setIsModalOpen(true);
   };
 
   const handleCloseModal = () => {
-    setIsModalOpen(false); 
+    setIsModalOpen(false);
   };
 
   const handleSelect = (id: number) => {
-    setSelectedLevel(id); 
-  };  
+    setSelectedLevel(id);
+  };
 
   return (
-    <div className='staking-countainer'>
-
-        <div className="home-top-catlog">
-
-            <div className="home-user">
-                <div className="user-icon"></div>
-                <p className="home-user-p">UserName</p>
-            </div>
-
-            <div className="home-level">
-                <p className="level-p">Выбор уровня</p> <span onClick={handleOpenModal} className="arrow">&gt;</span>
-            </div>
+    <div className="staking-countainer">
+      <div className="home-top-catlog">
+        <div className="home-user">
+          <div className="user-icon"></div>
+          <p className="home-user-p">UserName</p>
         </div>
 
-        {isModalOpen && (
+        <div className="home-level">
+          <p className="level-p">Выбор уровня</p>
+          <span onClick={handleOpenModal} className="arrow">
+            &gt;
+          </span>
+        </div>
+      </div>
+
+      {isModalOpen && (
         <div className="modal-overlay" onClick={handleCloseModal}>
           <div className="modal" onClick={(e) => e.stopPropagation()}>
             <div className="levels-list">
+              <button className="level-btn-win">
+                Выбор уровня{" "}
+                <span
+                  onClick={handleCloseModal}
+                  className="level-btn-win-arrow"
+                >
+                  &gt;
+                </span>
+              </button>
 
-             <button className="level-btn-win">Выбор уровня <span onClick={handleCloseModal} className="level-btn-win-arrow">&gt;</span></button>
-
-             {level.map((level) => (
-              <Level selectedLevel={selectedLevel} onSelect={handleSelect} key={level.id} data={level} />
-             ))}
-
+              {level.map((level) => (
+                <Level
+                  selectedLevel={selectedLevel}
+                  onSelect={handleSelect}
+                  key={level.id}
+                  data={level}
+                />
+              ))}
             </div>
           </div>
         </div>
       )}
 
-        <div className='stak-balance'>
-
-            <div className='stak-balance-price'>
-                <p className="stak-balance-title">Current Balance</p>
-                <p className="stak-balance-num">$00.00</p>
-            </div>
-
-            <div className='stak-balance-line'></div>
-
-            <div className="stak-btn-div">
-
-                <div className="stak-first-div">
-                    <img src={deposit} alt="desposit" />
-                    <p className="deposit-p">Пополнить</p>
-                </div>
-
-                <div className="stak-first-div">
-                    <img src={withdraw} alt="withdraw" />
-                    <p className="deposit-p">Вывод</p>
-                </div>
-            </div>
+      <div className="stak-balance">
+        <div className="stak-balance-price">
+          <p className="stak-balance-title">Current Balance</p>
+          <p className="stak-balance-num">$00.00</p>
         </div>
 
-        <p className="stak-hist-title">История транзакций</p>
-        <p className="stak-info-p">Пока нет транзакций</p>
+        <div className="stak-balance-line"></div>
 
+        <div className="stak-btn-div">
+          <div className="stak-first-div">
+            <img src={deposit} alt="desposit" />
+            <p className="deposit-p">Пополнить</p>
+          </div>
+
+          <div className="stak-first-div">
+            <img src={withdraw} alt="withdraw" />
+            <p className="deposit-p">Вывод</p>
+          </div>
+        </div>
+      </div>
+
+      <p className="stak-hist-title">История транзакций</p>
+      <p className="stak-info-p">Пока нет транзакций</p>
     </div>
-  )
+  );
 }
